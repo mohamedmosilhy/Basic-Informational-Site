fc1dff083a2007e65d5a61d7558280387983e194
Mohamed Mosilhy <93820559+mohamedmosilhy@users.noreply.github.com>
2025-11-29 15:40:48 +0200

    Implement server logic in index.js and remove api/server.js

---
+++ index.js
@@ -0,0 +1,35 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const server = http.createServer((req, res) => {
+  let file = "404.html";
+
+  if (req.url === "/") file = "index.html";
+  else if (req.url === "/about") file = "about.html";
+  else if (req.url === "/contact-me") file = "contact-me.html";
+  else if (req.url === "/styles.css") file = "styles.css";
+
+  const filePath = path.join(__dirname, file);
+  const ext = path.extname(filePath);
+
+  let type = "text/html";
+  if (ext === ".css") type = "text/css";
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      const notFound = path.join(__dirname, "404.html");
+      return fs.readFile(notFound, (_, nf) => {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.end(nf);
+      });
+    }
+
+    res.writeHead(200, { "Content-Type": type });
+    res.end(data);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Server running at http://localhost:3000");
+});
